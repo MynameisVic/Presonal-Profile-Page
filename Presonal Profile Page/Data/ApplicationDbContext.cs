17d3717b3128caceddba20df246a6b6823295046
ASUS <ASUS@VIT>
2024-07-06 19:19:16 +0700

    Add project files.

---
+++ Presonal Profile Page/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<UserProfile> UserProfiles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=userprofiles.db");
+    }
+}
